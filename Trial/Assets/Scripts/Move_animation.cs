928e93d1cb86bdf0835e6573f03f8954de19057c
MaxitoSama <marc.garcia.u@gmail.com>
2019-07-03 12:53:19 +0200

    PROTOTYPE

---
+++ Trial/Assets/Scripts/Move_animation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_animation : MonoBehaviour {
+
+    public float rotation = 1.0f;
+    public float total_rot = 1.0f;
+    private bool ret_A = false;
+    private bool ret_D = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("Movement Animation script added to:" + gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!ret_A && !ret_D)
+        {
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && total_rot < 45)
+            {
+                total_rot += rotation;
+                transform.Rotate(0, rotation, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && total_rot < 45)
+            {
+                total_rot += rotation;
+                transform.Rotate(0, -rotation, 0);
+            }
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                ret_D = true;
+               
+            }
+
+            if (Input.GetKeyUp(KeyCode.A) )
+            {
+                ret_A = true;
+            }
+        }
+        else
+        {
+            if(ret_A && total_rot > 0)
+            {
+                transform.Rotate(0, +rotation, 0);
+                total_rot -= rotation;
+            }
+            else if(ret_D && total_rot > 0)
+            {
+                transform.Rotate(0, -rotation, 0);
+                total_rot -= rotation;
+            }
+            else
+            {
+                ret_A = ret_D = false;
+            }
+        }
+        
+    }
+}
---
+++ Trial/Assets/Scripts/bike_2.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BikeInfo
+{
+    public WheelCollider frontWheel;
+    public GameObject frontWheelMesh;
+    public WheelCollider rareWheel;
+    public GameObject rareWheelMesh;
+    public bool motor;
+    public bool steering;
+}
+
+public class bike_2 : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpSpeed = 6.0f;
+    public float gravity = 20.0f;
+    public float rotation = 1.0f;
+    private Vector3 move = Vector3.zero;
+
+    public List<BikeInfo> bikeInfo;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+
+
+    private float total_rot = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Bike movement script added to:" + gameObject.name);
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        //        Rigidbody controller = GetComponent<Rigidbody>();
+
+
+        if (controller.isGrounded)
+        {
+            move = new Vector3(0, 0, -Input.GetAxis("Vertical"));
+            move = transform.TransformDirection(move);
+            move *= speed;
+            if (Input.GetButton("Jump"))
+                move.y = jumpSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Q) && total_rot<45)
+        {
+            total_rot += rotation;
+            transform.Rotate(rotation, 0,0);
+        }
+
+        if(!Input.GetKey(KeyCode.Q) && total_rot>0)
+        {
+            transform.Rotate(-rotation, 0,0);
+            total_rot -= rotation;
+        }
+
+        move.y -= gravity * Time.deltaTime;
+        controller.Move(move*Time.deltaTime);
+
+        if(!Input.GetKey(KeyCode.Q) && total_rot<=0)
+        {
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotation, 0);
+        }
+        else if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotation, 0);
+        }
+
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach(BikeInfo bikeInfo in bikeInfo)
+        {
+            if(bikeInfo.steering)
+            {
+                bikeInfo.frontWheel.steerAngle = steering;
+            }
+        }
+    }
+
+}
