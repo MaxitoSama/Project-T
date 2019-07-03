39b8bbc6ce235a766859e309d96f19c5a0eb7521
MaxitoSama <marc.garcia.u@gmail.com>
2019-07-03 16:59:51 +0200

    CLEANING CODE

---
+++ Trial/Assets/Scripts/Camera_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_script : MonoBehaviour
+{
+    public GameObject Bike;
+
+    public float speed = 5;
+
+    Vector3 offset;
+    Vector3 playerPrevPos, playerMoveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Bike.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Bike.transform.position + new Vector3(Bike.transform.forward.x*offset.magnitude,offset.y, Bike.transform.forward.z*offset.magnitude);
+
+        transform.LookAt(Bike.transform.position);
+    }
+}
